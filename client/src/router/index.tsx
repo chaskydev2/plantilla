04f5c1e6a1c401dc5804eaf1bdc568bff8772aab
digrajeda <di.grajeda@gmail.com>
2025-08-15 16:13:13 -0400

    chore(client): limpiar rutas públicas y admin (sin announcements/events/courses/payments)

--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -2,8 +2,9 @@ import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import variables from "@/core/config/variables";
 import { useDispatch } from "react-redux";
-import { getMe } from '@/core/reducer/auth.reducer';
+import { getMe } from "@/core/reducer/auth.reducer";
 import type { AppDispatch } from "@/store";
+
 import AppLayoutAdmin from "@/components/layout/AppLayoutAdmin";
 import { ScrollToTop } from "@/components/common/ScrollToTop";
 import AppLayoutWeb from "@/components/layout/AppLayoutWeb";
@@ -11,24 +12,42 @@ import AppLayoutWeb from "@/components/layout/AppLayoutWeb";
 import SignIn from "@/pages/AuthPages/SignIn";
 import NotFound from "@/pages/OtherPage/NotFound";
 import Unauthorized from "@/pages/OtherPage/Unauthorized";
-import HomePage from '@/pages/WebPage/HomePage';
+
+// Web (público)
+import HomePage from "@/pages/WebPage/HomePage";
 import ContactPage from "@/pages/WebPage/ContactPage";
-import EventsPage from "@/pages/WebPage/EventsPage";
+// 🧹 Eliminado: EventsPage
+// 🧹 Eliminado: CoursesPage (público)
+// 🧹 Eliminado: CertificationsPage (público)
+import Historypage from "@/pages/WebPage/Historypage";
+import Directivapage from "@/pages/WebPage/DirectivePage";
+import Requisitospage from "@/pages/WebPage/RequirementsPage";
+import Estatutospage from "@/pages/WebPage/StatutesPage";
+import Visionpage from "@/pages/WebPage/Visionpage";
+import Conveniospage from "@/pages/WebPage/AgreementsPage";
+import Renovationpage from "@/pages/WebPage/RenovationPage";
+import Visadopage from "@/pages/WebPage/Visadopage";
+import Formulariopage from "@/pages/WebPage/FormPage";
+import AffiliatesPage from "@/pages/WebPage/AffiliatesPage";
+
 import PrivateRoute from "./PrivateRoute";
 import GuestRoute from "./GuestRoute";
 
+// Admin
 import HomeAdmin from "@/pages/admin/home/Home";
 import UserPage from "@/pages/admin/users/Main";
 import UserShowPage from "@/pages/admin/users/Show";
 import RolPage from "@/pages/admin/roles/Main";
 import RolPermissionPage from "@/pages/admin/roles/permissions/Main";
 import PermissionPage from "@/pages/admin/permissions/Main";
-import AnnouncementPage from "@/pages/admin/announcements/Main";
-import EventTypePage from "@/pages/admin/event-types/Main";
-import EventPage from "@/pages/admin/events/Main";
-import CoursePage from "@/pages/admin/courses/Main";
-import PaymentPage from "@/pages/admin/monthlypay/Main";
-import PaymentReport from "@/pages/admin/monthlypay/Report";
+// 🧹 Eliminados (admin):
+// import AnnouncementPage from "@/pages/admin/announcements/Main";
+// import EventTypePage from "@/pages/admin/event-types/Main";
+// import EventPage from "@/pages/admin/events/Main";
+// import CoursePage from "@/pages/admin/courses/Main";
+// import PaymentPage from "@/pages/admin/monthlypay/Main";
+// import PaymentReport from "@/pages/admin/monthlypay/Report";
+
 import HistoryAdminPage from "@/pages/admin/histories/Main";
 import ContactAdminPage from "@/pages/admin/contacts/Main";
 import BeginningPage from "@/pages/admin/beginnings/Main";
@@ -40,19 +59,6 @@ import NewsletterPage from "@/pages/admin/newsletters/Main";
 import FaqPage from "@/pages/admin/faqs/Main";
 import BannerPage from "@/pages/admin/banners/Main";
 import SocialNetworkPage from "@/pages/admin/social-networks/Main";
-
-import Historypage from "@/pages/WebPage/Historypage";
-import Directivapage from "@/pages/WebPage/DirectivePage";
-import Requisitospage from "@/pages/WebPage/RequirementsPage";
-import Estatutospage from "@/pages/WebPage/StatutesPage";
-import Visionpage from "@/pages/WebPage/Visionpage";
-import Coursespage from "@/pages/WebPage/CoursesPage";
-import Conveniospage from "@/pages/WebPage/AgreementsPage";
-import Renovationpage from "@/pages/WebPage/RenovationPage";
-import Visadopage from "@/pages/WebPage/Visadopage";
-import Certificacionespage from "@/pages/WebPage/CertificationsPage";
-import Formulariopage from "@/pages/WebPage/FormPage";
-import AffiliatesPage from "@/pages/WebPage/AffiliatesPage";
 
 import UserProfiles from "@/pages/admin/UserProfiles";
 
@@ -73,9 +79,7 @@ export default function App() {
   }, [dispatch]);
 
   if (checkingAuth) {
-    return (
-      <LoaderScreen />
-    );
+    return <LoaderScreen />;
   }
 
   return (
@@ -88,8 +92,7 @@ export default function App() {
           <Route element={<AppLayoutWeb />}>
             <Route path="/" element={<HomePage />} />
             <Route path="/contacto" element={<ContactPage />} />
-            <Route path="/cursos" element={<Coursespage />} />
-            <Route path="/events" element={<EventsPage />} />
+            {/* 🧹 Eliminadas rutas públicas: /cursos, /events, /certificacion_trabajo */}
             <Route path="/historia" element={<Historypage />} />
             <Route path="/mision" element={<Visionpage />} />
             <Route path="/convenios" element={<Conveniospage />} />
@@ -99,7 +102,6 @@ export default function App() {
             <Route path="/renovacion_datos" element={<Renovationpage />} />
             <Route path="/visado_planos" element={<Visadopage />} />
             <Route path="/afiliados" element={<AffiliatesPage />} />
-            <Route path="/certificacion_trabajo" element={<Certificacionespage />} />
             <Route path="/formulario_solicitud" element={<Formulariopage />} />
           </Route>
 
@@ -108,28 +110,35 @@ export default function App() {
             <Route element={<AppLayoutAdmin />}>
               <Route path="/admin" element={<HomeAdmin />} />
               <Route path="/admin/perfil" element={<UserProfiles />} />
+
+              {/* Usuarios / Roles / Permisos (se mantienen) */}
               <Route path="/admin/usuarios" element={<UserPage />} />
               <Route path="/admin/usuarios/:id" element={<UserShowPage />} />
               <Route path="/admin/roles" element={<RolPage />} />
               <Route path="/admin/roles/:id/permisos" element={<RolPermissionPage />} />
               <Route path="/admin/permisos" element={<PermissionPage />} />
-              <Route path="/admin/comunicados" element={<AnnouncementPage />} />
-              <Route path="/admin/tipo_eventos" element={<EventTypePage />} />
-              <Route path="/admin/eventos" element={<EventPage />} />
-              <Route path="/admin/cursos" element={<CoursePage />} />
-              <Route path="/admin/montlypay" element={<PaymentPage />} />
-              <Route path="/admin/montlypayreport" element={<PaymentReport />} />
-              <Route path="/admin/historias" element={<HistoryAdminPage/>} />
-              <Route path="/admin/contactos" element={<ContactAdminPage/>} />
-              <Route path="/admin/principios" element={<BeginningPage/>} />
-              <Route path="/admin/valores_morales" element={<MoralValuePage/>} />
-              <Route path="/admin/directiva" element={<DirectivityAdminPage/>} />
-              <Route path="/admin/requisitos" element={<RequirementAdminPage/>} />
-              <Route path="/admin/acuerdos" element={<AgreementAdminPage/>} />
-              <Route path="/admin/consultas" element={<NewsletterPage/>} />
-              <Route path="/admin/preguntas_frecuentes" element={<FaqPage/>} />
-              <Route path="/admin/banners" element={<BannerPage/>} />
-              <Route path="/admin/redes_sociales" element={<SocialNetworkPage/>} />
+
+              {/* 🧹 Eliminadas rutas admin:
+                  /admin/comunicados
+                  /admin/tipo_eventos
+                  /admin/eventos
+                  /admin/cursos
+                  /admin/montlypay
+                  /admin/montlypayreport
+              */}
+
+              {/* Web (admin) que sí permanece */}
+              <Route path="/admin/historias" element={<HistoryAdminPage />} />
+              <Route path="/admin/contactos" element={<ContactAdminPage />} />
+              <Route path="/admin/principios" element={<BeginningPage />} />
+              <Route path="/admin/valores_morales" element={<MoralValuePage />} />
+              <Route path="/admin/directiva" element={<DirectivityAdminPage />} />
+              <Route path="/admin/requisitos" element={<RequirementAdminPage />} />
+              <Route path="/admin/acuerdos" element={<AgreementAdminPage />} />
+              <Route path="/admin/consultas" element={<NewsletterPage />} />
+              <Route path="/admin/preguntas_frecuentes" element={<FaqPage />} />
+              <Route path="/admin/banners" element={<BannerPage />} />
+              <Route path="/admin/redes_sociales" element={<SocialNetworkPage />} />
             </Route>
           </Route>
 
